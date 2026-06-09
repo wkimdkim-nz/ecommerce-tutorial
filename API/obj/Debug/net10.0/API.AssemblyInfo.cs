0d5c049cc9f04c72a6dd850a5043881a4bb593a2
Derek Kim <wkimdkim@gmail.com>
2026-06-09 20:13:07 +1200

    Entityframework SQLLite and design Nuget

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51fc6728baef250a154d1273bf5fd54f7ec78999")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5294972bb5436fdee1686a042478f56a1ecc848f")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
